88418c2b708c735d10ff06654cc4db4527cd4c7f
hamza raouzi <hamza.raouzi2016@gmail.com>
2022-03-27 19:05:27 +0000

    debugging training

---
+++ DataPreparers/supervisedImageClassificationDataset.py
@@ -0,0 +1,34 @@
+import torch
+from  torch.utils.data import Dataset
+import os
+from  PIL import Image 
+from torchvision import transforms
+import pandas as pd
+
+
+class SupervisedImageClassicationDataset(Dataset):
+
+    def __init__(self, img_dir:str, csv_file:str, target_column:str,split:str, sampling:str
+        ,transform:transforms):
+        
+
+        self.df = pd.read_csv(csv_file)
+        self.target_column = target_column
+        self.img_dir = img_dir
+        self.transform = transform
+        
+    
+
+        
+    def __len__(self):
+        return len(self.df)
+    
+    def __getitem__(self, idx):
+        
+        y = self.df.loc[idx, self.target_column]
+        image_name = self.df.loc[idx, image_name]
+        
+        img_path = os.path.join(self.img_dir, image_name)
+        img = Image.open(img_path).convert('RGB')
+
+        return self.transform(img), torch.tensor(y)        
